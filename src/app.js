1f0f8ae74904df12509ad7b5b2ea5a90a3111816
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-10-26 15:55:42 -0400

    Addded Javascript, edited About Me
    Added Javascript to stop links from being clickable when active. Added education and background to About Me section. Adjusted footer so it was the same across all pages.

---
+++ src/app.js
@@ -0,0 +1,6 @@
+
+function stopActive(event) {event.preventDefault();
+activeLink.classList.add("active");}
+
+let activeLink = document.querySelector(".active");
+activeLink.addEventListener("click", stopActive);
