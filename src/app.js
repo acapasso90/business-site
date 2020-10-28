526ccd737ca4d433b456fa3f153290d9cfec4bc6
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-10-27 21:59:22 -0400

    added link sidebar to contact page. button css
    changed hover button css to darken button. Added sidebar of links to contact page. Began position adjustment

--- src/app.js
+++ src/app.js
@@ -5,10 +5,3 @@ activeLink.classList.add("active");}
 let activeLink = document.querySelector(".active");
 activeLink.addEventListener("click", stopActive);
 
-function submitEmail(event) {event.preventDefault();
-let firstname = document.querySelector("#firstnameSubmit").value;
-alert(`Thank you, ${firstname}. I will be responding to you as soon as possible!`)
-}
-
-let submitMessage = document.querySelector(".inputBars");
-submitMesssage.addEventListener("submit", submitEmail);
