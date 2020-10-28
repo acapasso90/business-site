aed9f12103327e0d85b39b5de30c29fcd2d4e974
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-10-27 21:29:06 -0400

    Readjusted positioning with floats
    Repositioned about me page with floats. Added shadows to text on front page. Began work on javascript to adjust behaivor on submit message.

--- src/app.js
+++ src/app.js
@@ -4,3 +4,11 @@ activeLink.classList.add("active");}
 
 let activeLink = document.querySelector(".active");
 activeLink.addEventListener("click", stopActive);
+
+function submitEmail(event) {event.preventDefault();
+let firstname = document.querySelector("#firstnameSubmit").value;
+alert(`Thank you, ${firstname}. I will be responding to you as soon as possible!`)
+}
+
+let submitMessage = document.querySelector(".inputBars");
+submitMesssage.addEventListener("submit", submitEmail);
