43abaf381fa6a7957ff124404936edeaefee07d7
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-11-23 14:26:14 -0500

    Added changing text to index.html
    Added text changed to "basedIn" on index.html to list current skills.

--- src/app.js
+++ src/app.js
@@ -5,3 +5,11 @@ activeLink.classList.add("active");}
 let activeLink = document.querySelector(".active");
 activeLink.addEventListener("click", stopActive);
 
+var text = ["React.js", "JavaScript", "CSS", "HTML", "Responsive Design", "Front-end Web Developer based in CT"];
+var counter = 0;
+var basedIn = document.querySelector(".basedIn");
+setInterval(change, 2000);
+function change() {
+basedIn.innerHTML = text[counter];
+    counter++;
+    if(counter >= text.length) { counter = 0; }}
