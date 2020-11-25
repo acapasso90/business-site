785b31cc153a4f5beaa5b9ccd6c8b5cf587ddfdc
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-11-25 15:14:36 -0500

    Added winter div art to portfolio page
    Added winter div art to portfolio page

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ activeLink.classList.add("active");}
 let activeLink = document.querySelector(".active");
 activeLink.addEventListener("click", stopActive);
 
-var text = ["React.js", "JavaScript", "CSS", "HTML", "Responsive Design", "Front-end Web Developer based in CT"];
+var text = ["React.js", "JavaScript", "CSS", "HTML", "Web Design", "Front-end Web Developer based in CT"];
 var counter = 0;
 var basedIn = document.querySelector(".basedIn");
 setInterval(change, 2000);
