807608704c69efdcae68dadda59869904524a1fa
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-11-29 14:31:51 -0500

    Applied JavaScript to change CSS styling on "basedIn" array dependent on screen size
    Applied JavaScript to change CSS styling on "basedIn" array dependent on screen size. Letter spacing is decreased on "Front-end developer based in CT" on mobile screen sizes

--- src/indexapp.js
+++
@@ -1,8 +0,0 @@
-var text = ["React.js", "JavaScript", "Responsive Web Design", "CSS", "HTML", "Front-end Web Developer based in CT"];
-var counter = 0;
-var basedIn = document.querySelector(".basedIn");
-setInterval(change, 2000);
-function change() {
-basedIn.innerHTML = text[counter];
-counter++;
-if(counter >= text.length) { counter = 0; };}
