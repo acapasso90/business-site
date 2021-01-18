e638b4e829df757e0c5b3e01c1781f133cc755bc
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-01-18 10:40:25 -0500

    Updated to portfolio site v2
    Updated to portfolio site v2

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
-function stopActive(event) {event.preventDefault();
-activeLink.classList.add("active");}
-
-const activeLink = document.querySelector(".active");
-activeLink.addEventListener("click", stopActive);
 
 const htmllabel = document.getElementById("html");
 const html = document.querySelector(".fa-html5");
@@ -15,7 +10,7 @@ const react = document.querySelector(".fa-react");
 const githublabel = document.getElementById("github");
 const github = document.querySelector(".fa-github-square");
 const bootstraplabel = document.getElementById("bootstrap");
-const bootstrap = document.querySelector(".fa-bootstrap");
+const bootstrapIcon = document.querySelector(".fa-bootstrap");
 const npmlabel = document.getElementById("npm");
 const npm = document.querySelector(".fa-npm");
 const visuallabel = document.getElementById("visualstudiocode");
@@ -40,9 +35,9 @@ github.addEventListener("mouseenter", e => {
         githublabel.style.opacity = '100%';})
 github.addEventListener("mouseleave", e => {
     githublabel.style.opacity = '0%';});
-bootstrap.addEventListener("mouseenter", e => {
+    bootstrapIcon.addEventListener("mouseenter", e => {
     bootstraplabel.style.opacity = '100%';})
-bootstrap.addEventListener("mouseleave", e => {
+    bootstrapIcon.addEventListener("mouseleave", e => {
     bootstraplabel.style.opacity = '0%';});
 npm.addEventListener("mouseenter", e => {
         npmlabel.style.opacity = '100%';})
@@ -51,7 +46,43 @@ npm.addEventListener("mouseleave", e => {
 visual.addEventListener("mouseenter", e => {
         visuallabel.style.opacity = '100%';})
 visual.addEventListener("mouseleave", e => {
-    visuallabel.style.opacity = '0%';});
+    visuallabel.style.opacity = '0%';})
 
 
+    var myCarousel = document.querySelector('#myCarousel')
+    var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 6000,
+      wrap: false
+    })
 
+
+    const next = document.getElementById('next')
+    const previous = document.getElementById('previous')
+
+    function nextSlide(){$('.carousel').carousel('next')}
+    function previousSlide(){$('.carousel').carousel('prev')}
+
+    next.addEventListener("click", nextSlide)
+    previous.addEventListener("click", previousSlide)
+
+
+    let options = {
+        root: document.querySelector('#scrollArea'),
+        rootMargin: '0px',
+        threshold: 0.25
+      }
+      
+const headers = document.querySelectorAll('.headers')
+
+observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0){
+        entry.target.classList.add('headerShown');
+        observer.unobserve(entry.target);}
+        else {
+            entry.target.classList.remove('headerShown');
+        }
+    });
+  });
+
+    headers.forEach(h1 => { observer.observe(h1)});
