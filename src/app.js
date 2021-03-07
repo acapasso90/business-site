1e3f0bdea4f66b12d998be47874266c12408b844
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-03-07 15:29:54 -0500

    Removed 3 preview images and added gif preview into hiddenInfo
    Removed 3 preview images and added gif preview into hiddenInfo

--- src/app.js
+++ src/app.js
@@ -117,13 +117,4 @@ blackout.classList.add('blackoutShown');
 
 // on click triggers showPopup function
 const popupLink = document.querySelector('.popupLink');
-popupLink.addEventListener("click", showPopup)
-
-$(document).ready(function(){
-    $(".dropdown").hover(function(){
-        var dropdownMenu = $(this).children(".dropdown-menu");
-        if(dropdownMenu.is(":visible")){
-            dropdownMenu.parent().toggleClass("open");
-        }
-    });
-});     
+popupLink.addEventListener("click", showPopup);  
