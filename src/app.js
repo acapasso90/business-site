b6d8c5c7755f498d95f8ca7336a3898cd6db3d9c
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-06-04 16:21:46 -0400

    cleanup
    cleanup

--- src/app.js
+++ src/app.js
@@ -1,62 +1,62 @@
 
 
 //Sets intervel for carousel to change slides
-    var myCarousel = document.querySelector('#myCarousel')
-    var carousel = new bootstrap.Carousel(myCarousel, {
-      interval: 1599,
-      wrap: false
-    })
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 1599,
+  wrap: false
+})
 
 //Adds nextSlide/previousSlide functionality to next and previous arrows on carousel
-    const next = document.getElementById('next')
-    const previous = document.getElementById('previous')
+const next = document.getElementById('next')
+const previous = document.getElementById('previous')
 
-    function nextSlide(){$('.carousel').carousel('next')}
-    function previousSlide(){$('.carousel').carousel('prev')}
+function nextSlide(){$('.carousel').carousel('next')}
+function previousSlide(){$('.carousel').carousel('prev')}
 
-    next.addEventListener("click", nextSlide)
-    previous.addEventListener("click", previousSlide)
+next.addEventListener("click", nextSlide)
+previous.addEventListener("click", previousSlide)
 
 
 //Sets Intersection_Observer_API margin, threshold and area 
-    let options = {
-        root: document.querySelector('#scrollArea'),
-        rootMargin: '0px',
-        threshold: 0.25
-      }
-      
+let options = {
+    root: document.querySelector('#scrollArea'),
+    rootMargin: '0px',
+    threshold: 0.25
+  }
+  
 const headers = document.querySelectorAll('.headers')
 
 //Sets Intersection_Observer_API to add headerShown classlist when scrolled to
 observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.intersectionRatio > 0){
-        entry.target.classList.add('headerShown');
-        observer.unobserve(entry.target);}
-        else {
-            entry.target.classList.remove('headerShown');
-        }
-    });
-  });
+entries.forEach(entry => {
+    if (entry.intersectionRatio > 0){
+    entry.target.classList.add('headerShown');
+    observer.unobserve(entry.target);}
+    else {
+        entry.target.classList.remove('headerShown');
+    }
+});
+});
 //Observes all the headers
-    headers.forEach(h1 => { observer.observe(h1)});
+headers.forEach(h1 => { observer.observe(h1)});
 
 
 // Adds display:none from hiddenInfo and removes shown class
 function closePopup(){
-    const hidden = document.querySelector('.shown');
-    const blackout = document.querySelector('.blackout');
-    const noScroll = document.querySelector('html');
-    noScroll.classList.remove('popUpBody');
-    hidden.classList.remove('shown');
-    hidden.classList.add('hiddenInfo');
-    blackout.classList.remove('blackoutShown');
+const hidden = document.querySelector('.shown');
+const blackout = document.querySelector('.blackout');
+const noScroll = document.querySelector('html');
+noScroll.classList.remove('popUpBody');
+hidden.classList.remove('shown');
+hidden.classList.add('hiddenInfo');
+blackout.classList.remove('blackoutShown');
 
-    }
+}
 
 
 
-    
+
 
 // on click closePopup
 const exitButton = document.querySelectorAll('.closeButton');
@@ -76,29 +76,40 @@ hidden.classList.add('shown');
 
 // Removes display:none from hiddenInfo and adds shown class
 function showPopupTwo(){
-    const hidden = document.getElementById('hiddenInfo2');
-    const noScroll = document.querySelector('html');
-    const blackout = document.querySelector('.blackout');
+const hidden = document.getElementById('hiddenInfo2');
+const noScroll = document.querySelector('html');
+const blackout = document.querySelector('.blackout');
 blackout.classList.add('blackoutShown');
-    noScroll.classList.add('popUpBody');
-    hidden.classList.remove('hiddenInfo');
-    hidden.classList.add('shown');
-    }
+noScroll.classList.add('popUpBody');
+hidden.classList.remove('hiddenInfo');
+hidden.classList.add('shown');
+}
 
 // Removes display:none from hiddenInfo and adds shown class
 function showPopupThree(){
-    const hidden = document.getElementById('hiddenInfo3');
-    const noScroll = document.querySelector('html');
-    const blackout = document.querySelector('.blackout');
+const hidden = document.getElementById('hiddenInfo3');
+const noScroll = document.querySelector('html');
+const blackout = document.querySelector('.blackout');
 blackout.classList.add('blackoutShown');
-    noScroll.classList.add('popUpBody');
-    hidden.classList.remove('hiddenInfo');
-    hidden.classList.add('shown');
-    }
+noScroll.classList.add('popUpBody');
+hidden.classList.remove('hiddenInfo');
+hidden.classList.add('shown');
+}
 
-    // Removes display:none from hiddenInfo and adds shown class
+// Removes display:none from hiddenInfo and adds shown class
 function showPopupFour(){
-    const hidden = document.getElementById('hiddenInfo4');
+const hidden = document.getElementById('hiddenInfo4');
+const noScroll = document.querySelector('html');
+const blackout = document.querySelector('.blackout');
+blackout.classList.add('blackoutShown');
+noScroll.classList.add('popUpBody');
+hidden.classList.remove('hiddenInfo');
+hidden.classList.add('shown');
+}
+
+// Adds display:none from hiddenInfo and removes shown class
+function showPopupFive(){
+    const hidden = document.getElementById('hiddenInfo5');
     const noScroll = document.querySelector('html');
     const blackout = document.querySelector('.blackout');
 blackout.classList.add('blackoutShown');
@@ -107,30 +118,19 @@ blackout.classList.add('blackoutShown');
     hidden.classList.add('shown');
     }
 
-    // Adds display:none from hiddenInfo and removes shown class
-    function showPopupFive(){
-        const hidden = document.getElementById('hiddenInfo5');
-        const noScroll = document.querySelector('html');
-        const blackout = document.querySelector('.blackout');
+// Adds display:none from hiddenInfo and removes shown class
+function showPopupSix(){
+    const hidden = document.getElementById('hiddenInfo6');
+    const noScroll = document.querySelector('html');
+    const blackout = document.querySelector('.blackout');
 blackout.classList.add('blackoutShown');
-        noScroll.classList.add('popUpBody');
-        hidden.classList.remove('hiddenInfo');
-        hidden.classList.add('shown');
-        }
-    
-    // Adds display:none from hiddenInfo and removes shown class
-    function showPopupSix(){
-        const hidden = document.getElementById('hiddenInfo6');
-        const noScroll = document.querySelector('html');
-        const blackout = document.querySelector('.blackout');
-blackout.classList.add('blackoutShown');
-        noScroll.classList.add('popUpBody');
-        hidden.classList.remove('hiddenInfo');
-        hidden.classList.add('shown');
-        }
-    
+    noScroll.classList.add('popUpBody');
+    hidden.classList.remove('hiddenInfo');
+    hidden.classList.add('shown');
+    }
 
-    
+
+
 
 
 // on click triggers showPopup function
@@ -156,4 +156,3 @@ popupLinkFive.addEventListener("click", showPopupFive);
 // on click triggers showPopup function
 const popupLinkSix = document.querySelector('.popupLinkSix');
 popupLinkSix.addEventListener("click", showPopupSix);  
-
