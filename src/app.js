e7471360fd4946acdae1a4868ece1cea014fa2d9
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-06-13 12:40:49 -0400

    Added hover effects to About Me icons, moved Blackout down
    Added hover effects to About Me icons, moved Blackout down

--- src/app.js
+++ src/app.js
@@ -156,3 +156,53 @@ popupLinkFive.addEventListener("click", showPopupFive);
 // on click triggers showPopup function
 const popupLinkSix = document.querySelector('.popupLinkSix');
 popupLinkSix.addEventListener("click", showPopupSix);  
+
+const htmllabel = document.getElementById("html");
+const html = document.querySelector(".fa-html5");
+const csslabel = document.getElementById("css");
+const css = document.querySelector(".fa-css3-alt");
+const javascriptlabel = document.getElementById("javascript");
+const javascript = document.querySelector(".fa-js-square");
+const bootstraplabel = document.getElementById("bootstrap");
+const bootstrapIcon = document.querySelector(".fa-bootstrap");
+const npmlabel = document.getElementById("npm");
+const npm = document.querySelector(".fa-npm");
+const visuallabel = document.getElementById("visualstudiocode");
+const visual = document.querySelector(".devicon-visualstudio-plain");
+const reactlabel = document.getElementById('react');
+const react = document.getElementById('reactIcon');
+const githublabel = document.getElementById("github");
+const github = document.getElementById("githubIcon");
+// "hover" effects for Currently Utilizing icons.
+html.addEventListener("mouseenter", e => {
+    htmllabel.style.opacity = '100%';})
+html.addEventListener("mouseleave", e => {
+    htmllabel.style.opacity = '0%';});
+css.addEventListener("mouseenter", e => {
+        csslabel.style.opacity = '100%';})
+css.addEventListener("mouseleave", e => {
+       csslabel.style.opacity = '0%';});
+javascript.addEventListener("mouseenter", e => {
+        javascriptlabel.style.opacity = '100%';})
+javascript.addEventListener("mouseleave", e => {
+    javascriptlabel.style.opacity = '0%';});
+react.addEventListener("mouseenter", e => {
+       reactlabel.style.opacity = '100%';})
+react.addEventListener("mouseleave", e => {
+    reactlabel.style.opacity = '0%';});
+github.addEventListener("mouseenter", e => {
+        githublabel.style.opacity = '100%';})
+github.addEventListener("mouseleave", e => {
+    githublabel.style.opacity = '0%';});
+    bootstrapIcon.addEventListener("mouseenter", e => {
+    bootstraplabel.style.opacity = '100%';})
+    bootstrapIcon.addEventListener("mouseleave", e => {
+    bootstraplabel.style.opacity = '0%';});
+npm.addEventListener("mouseenter", e => {
+        npmlabel.style.opacity = '100%';})
+npm.addEventListener("mouseleave", e => {
+    npmlabel.style.opacity = '0%';});
+visual.addEventListener("mouseenter", e => {
+        visuallabel.style.opacity = '100%';})
+visual.addEventListener("mouseleave", e => {
+    visuallabel.style.opacity = '0%';})
